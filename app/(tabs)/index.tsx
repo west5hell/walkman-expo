65cd64321a17db9ae9b6ccbdbb243869e3e8c7e2
Ponter Chia <dozeuto@gmail.com>
2025-05-26 17:59:29 +0800

    FlatList Example

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,56 @@
-import { Text, View } from "react-native";
+import React from "react";
+import { FlatList, StatusBar, StyleSheet, Text, View } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+
+const DATA = [
+  {
+    id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
+    title: "First Item",
+  },
+  {
+    id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
+    title: "Second Item",
+  },
+  {
+    id: "58694a0f-3da1-471f-bd96-145571e29d72",
+    title: "Third Item",
+  },
+];
+
+type ItemProps = { title: string };
+
+const Item = ({ title }: ItemProps) => (
+  <View style={styles.item}>
+    <Text style={styles.title}>{title}</Text>
+  </View>
+);
 
 export default function HomeScreen() {
   return (
     <SafeAreaProvider>
-      <SafeAreaView style={{ height: 100, flexDirection: "row" }}>
-        <View style={{ backgroundColor: "blue", flex: 0.2 }} />
-        <View style={{ backgroundColor: "red", flex: 0.4 }} />
-        <Text style={{ color: "pink" }}>Hello World!</Text>
+      <SafeAreaView>
+        <FlatList
+          data={DATA}
+          renderItem={({ item }) => <Item title={item.title} />}
+          keyExtractor={(item) => item.id}
+        />
       </SafeAreaView>
     </SafeAreaProvider>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+  },
+  item: {
+    backgroundColor: "#f9c2ff",
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 32,
+  },
+});
