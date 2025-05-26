be9f4f2f4dd9f761d0aa4d3cb2f8143432f57876
Ponter Chia <dozeuto@gmail.com>
2025-05-26 18:41:34 +0800

    ScrollView

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,27 +1,24 @@
 import React from "react";
-import { RefreshControl, ScrollView, StyleSheet, Text } from "react-native";
+import { ScrollView, StatusBar, StyleSheet, Text } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function HomeScreen() {
-  const [refreshing, setRefreshing] = React.useState(false);
-
-  const onRefresh = React.useCallback(() => {
-    setRefreshing(true);
-    setTimeout(() => {
-      setRefreshing(false);
-    }, 2_000);
-  }, []);
-
   return (
     <SafeAreaProvider>
-      <SafeAreaView style={styles.container}>
+      <SafeAreaView style={styles.container} edges={["top"]}>
         <ScrollView
-          contentContainerStyle={styles.scrollView}
-          refreshControl={
-            <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
-          }
+          style={styles.scrollView}
+          showsVerticalScrollIndicator={false}
         >
-          <Text>Pull down to see RefreshControl indicator</Text>
+          <Text style={styles.text}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </Text>
         </ScrollView>
       </SafeAreaView>
     </SafeAreaProvider>
@@ -31,11 +28,13 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingTop: StatusBar.currentHeight,
   },
   scrollView: {
-    flex: 1,
     backgroundColor: "pink",
-    alignItems: "center",
-    justifyContent: "center",
+  },
+  text: {
+    fontSize: 42,
+    padding: 12,
   },
 });
