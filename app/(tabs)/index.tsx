04055e8e2cf96c9fdfadfa9c6a144c388900b742
Ponter Chia <dozeuto@gmail.com>
2025-05-26 20:41:13 +0800

    InputAccessoryView

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,22 +1,35 @@
 import React, { useState } from "react";
-import { StyleSheet, Switch } from "react-native";
+import {
+  Button,
+  InputAccessoryView,
+  ScrollView,
+  StyleSheet,
+  TextInput,
+} from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
+const inputAccessoryViewID = "uniqueID";
+const initialText = "";
+
 export default function HomeScreen() {
-  const [isEnabled, setIsEnabled] = useState(false);
-  const toggleSwitch = () => setIsEnabled((previousState) => !previousState);
+  const [text, setText] = useState(initialText);
 
   return (
     <SafeAreaProvider>
-      <SafeAreaView style={styles.container} edges={["top"]}>
-        <Switch
-          trackColor={{ false: "#767577", true: "#81b0ff" }}
-          thumbColor={isEnabled ? "#f5dd4b" : "#f4f3f4"}
-          ios_backgroundColor="#3e3e3e"
-          onValueChange={toggleSwitch}
-          value={isEnabled}
-        />
+      <SafeAreaView style={styles.container}>
+        <ScrollView keyboardDismissMode="interactive">
+          <TextInput
+            style={styles.textInput}
+            inputAccessoryViewID={inputAccessoryViewID}
+            onChangeText={setText}
+            value={text}
+            placeholder={"Please type here…"}
+          />
+        </ScrollView>
       </SafeAreaView>
+      <InputAccessoryView nativeID={inputAccessoryViewID}>
+        <Button onPress={() => setText(initialText)} title="Clear text" />
+      </InputAccessoryView>
     </SafeAreaProvider>
   );
 }
@@ -25,6 +38,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
-    justifyContent: "center",
+    paddingHorizontal: 20,
+  },
+  textInput: {
+    padding: 16,
+    borderColor: "black",
+    borderWidth: 1,
   },
 });
