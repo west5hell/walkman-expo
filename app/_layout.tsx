59174faa6925cc8d88c9af863a25614eafa1bec2
Ponter Chia <dozeuto@gmail.com>
2025-08-03 01:04:07 +0800

    updated

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,19 @@
+import { useColorScheme } from "@/hooks/useColorScheme.web";
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 
 export default function RootLayout() {
+  const colorScheme = useColorScheme();
   return (
-    <React.Fragment>
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+      <Stack></Stack>
       <StatusBar style="auto" />
-      <Stack />
-    </React.Fragment>
+    </ThemeProvider>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,47 +1,26 @@
-import { LinearGradient } from "expo-linear-gradient";
 import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
-export default function App() {
+export default function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <LinearGradient
-        // Background Linear Gradient
-        colors={["rgba(0,0,0,0.8)", "transparent"]}
-        style={styles.background}
-      />
-      <LinearGradient
-        // Button Linear Gradient
-        colors={["#4c669f", "#3b5998", "#192f6a"]}
-        style={styles.button}
-      >
-        <Text style={styles.text}>Sign in with Facebook</Text>
-      </LinearGradient>
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView style={{ flex: 1 }}>
+        <View style={styles.container}>
+          <Text style={styles.text}>Content is in safe area.</Text>
+        </View>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
     justifyContent: "center",
-    backgroundColor: "orange",
-  },
-  background: {
-    position: "absolute",
-    left: 0,
-    right: 0,
-    top: 0,
-    height: 300,
-  },
-  button: {
-    padding: 15,
+    // alignContent: "center",
     alignItems: "center",
-    borderRadius: 5,
   },
   text: {
-    backgroundColor: "transparent",
-    fontSize: 15,
-    color: "#fff",
+    color: "red",
   },
 });
